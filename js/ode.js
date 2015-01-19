c467f6b0ce1a697a990b0e741d4b55b754f335e0
oss6 <ossedb@gmail.com>
2015-01-19 00:00:01 +0000

    Finished euler method

---
+++ js/ode.js
@@ -0,0 +1,35 @@
+var ode = (function () {
+
+    var $ = {};
+    
+    $.euler = function (init, h, n, f) {
+        var x0 = init['x0'],
+            y0 = init['y0'],
+            xs = [x0],
+            ys = [y0];
+
+        // Create xs
+        for (var x = 1; x <= n; x++)
+            xs.push(x0 + x * h);
+
+        // Create ys
+        for (var i = 0; i < n; i++) {
+            var xn = xs[i],
+                yn = ys[i];
+
+            ys.push(yn + (h * f(xn, yn)))
+        }
+
+        return {
+            'xs': xs,
+            'ys': ys
+        }
+    };
+
+    $.midPoint = function () {
+
+    };
+
+    return $;
+
+})();
